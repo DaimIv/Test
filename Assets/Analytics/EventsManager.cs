ca17d1e7dc41f1c1fb9f957e704e87efe1cd6ac5
DaimIv <36898023+DaimIv@users.noreply.github.com>
2020-10-30 17:06:31 +0300

    Initial commit

---
+++ Assets/Analytics/EventsManager.cs
@@ -0,0 +1,89 @@
+﻿using Assets.Analytics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EventsManager : MonoBehaviour
+{
+    public string serverURL = "";
+    public List<POSTMessage> events;
+    public int cooldownBeforeSend = 10;
+    private  Timer sendTimer;
+
+    private void Awake()
+    {
+       
+        if(PlayerPrefs.HasKey("messages"))
+        {
+            events = JsonUtility.FromJson<List<POSTMessage>>(PlayerPrefs.GetString("messages"));
+        }
+        
+        if (events == null)
+        {
+            events = new List<POSTMessage>();
+
+        }
+        sendTimer = new Timer(SendPOST, null, 0, cooldownBeforeSend);
+    }
+    void SendPOST(object unused)
+    {
+        if (events.Count > 0)
+        {
+            StartCoroutine(SendRequest(serverURL, UnityEngine.JsonUtility.ToJson(events)));
+           
+        }
+    }
+    void MessageSent()
+    {
+        events.Clear();
+        SaveCurrentMessages();
+    }
+
+    void SaveCurrentMessages()
+    {
+        PlayerPrefs.SetString("messages",UnityEngine.JsonUtility.ToJson(events));
+    }
+
+    public void TrackEvent(POSTMessage message)
+    {
+        events.Add(message);
+        SaveCurrentMessages();
+    }
+    private void Start()
+    {
+        //Example
+
+        TrackEvent(new POSTMessage(EventsType.PayInGame, new object[2] { 500, "999222" }));
+
+    }
+
+
+
+
+
+
+
+
+
+    IEnumerator SendRequest(string url, string json)
+    {
+        var request = new UnityWebRequest(url, "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        request.uploadHandler = new UploadHandlerRaw(jsonToSend);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        
+        yield return request.SendWebRequest();
+
+
+        if (request.downloadHandler.text == "200 OK")
+        {
+            MessageSent();
+        }
+
+
+    }
+}
---
+++ Assets/Analytics/POSTMessage.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Analytics
+{
+   public class POSTMessage
+    {
+        public string type { get;private set; }
+        public object data { get;private set; }
+
+        private const string assemblyPath = "Assets.Analytics.";
+
+        public POSTMessage(string type,params object[] args)
+        {
+            this.type = type;
+            Type messageType = Type.GetType(assemblyPath+type, false, true);
+            data = Activator.CreateInstance(messageType);
+            if(data is ICustomParams && args != null)
+            {
+                MethodInfo method = messageType.GetMethod("AddMoreInfo");
+                method.Invoke(data, args);
+            }
+        }
+
+    }
+
+    public class level_start : MessageData, ILevelEvent
+    {
+        public int level { get;  set; } 
+        public level_start()
+        {
+            level = Application.loadedLevel;
+        }
+    }
+
+    public class level_win : MessageData, ILevelEvent
+    {
+        public int level { get;  set; }
+        public level_win()
+        {
+            level = Application.loadedLevel;
+        }
+    }
+    public class level_fail : MessageData, ILevelEvent
+    {
+        public int level { get; set; }
+        public level_fail() 
+        {
+            level = Application.loadedLevel;
+        }
+    }
+    public class pay_in_game : MessageData, IPayEvent, ICustomParams
+    {
+       
+        public int price { get ; set ; }
+        public string id { get; set ; }
+
+        public void AddMoreInfo(object[] args)
+        {
+            price = Convert.ToInt32(args[0]);
+            id = args[1].ToString();
+        }
+    }
+
+
+    public class MessageData
+    {
+        public long time { get; private set; }
+        public string version { get; private set; }
+
+        public MessageData()
+        {
+            time = new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds();
+            version = Application.version;
+        }
+    }
+
+    /// <summary>
+    /// This class contains string event -  identification for server
+    /// </summary>
+    public static class EventsType
+    {
+        public const string LevelStart = "level_start";
+        public const string LevelWin = "level_win";
+        public const string LevelFail = "level_fail";
+        public const string PayInGame = "pay_in_game";
+
+        
+    }
+
+    public interface ILevelEvent
+    {
+         int level { get;   set; }
+    }
+
+    public interface IPayEvent
+    {
+        int price { get; set; }
+        string id { get; set; }
+
+      
+    }
+
+    public interface ICustomParams
+    {
+        void AddMoreInfo(object[] args);
+    }
+}
